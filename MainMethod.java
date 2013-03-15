0ba046b033f827892a3574dac935002ae5dae244
dunderjeep <dunderjeep@gmail.com>
2013-03-15 16:13:47 -0400

    moved constants for sleep to userjob

--- MainMethod.java
+++ MainMethod.java
@@ -1,23 +1,13 @@
 public class MainMethod {
 
-	public static final int MIN_CPU_TIME = 100;
-	public static final int MAX_CPU_TIME = 1000;
-	public static final int MIN_IO_TIME = 1;
-	public static final int MAX_IO_TIME = 200;
-
   	public static void main (String argv[]) {
 		
 	UserJob UJ; 
 	UserJob.Job job;  
 	
 	for (int i=0; i<6; i++) {
-		if (i % 2 == 0) {
-			int CPUtime = MIN_CPU_TIME + (int)(Math.random() * ((MAX_CPU_TIME - MIN_CPU_TIME) + 1));
-			UJ = new UserJob(i, UserJob.Job.CPU_BOUND, CPUtime);
-		} else {
-			int IOtime = MIN_IO_TIME + (int)(Math.random() * ((MAX_IO_TIME - MIN_IO_TIME) + 1));				
-			UJ = new UserJob(i, UserJob.Job.IO_BOUND, IOtime);
-		}	
+		if (i % 2 == 0) UJ = new UserJob(i, UserJob.Job.CPU_BOUND);
+		else UJ = new UserJob(i, UserJob.Job.IO_BOUND);
 		UJ.start(); 
 	}
 	System.out.println("This is main speaking");
--- UserJob.java
+++ UserJob.java
@@ -1,4 +1,10 @@
 public class UserJob extends Thread {
+
+	public static final int MIN_CPU_TIME = 100;
+	public static final int MAX_CPU_TIME = 1000;
+	public static final int MIN_IO_TIME = 1;
+	public static final int MAX_IO_TIME = 200;
+
 	int myName;
 	int sleepytime;
 	int trackNumber;
@@ -6,11 +12,13 @@ public class UserJob extends Thread {
 	Job myJob;
 	DiskDrive DD;
 	
-	public UserJob(int name, Job job, int time) {
+	public UserJob(int name, Job job) {
 		myName = name;	
 		myJob = job;
-		sleepytime = time;
+		if (job == Job.CPU_BOUND) sleepytime = MIN_CPU_TIME + (int)(Math.random() * ((MAX_CPU_TIME - MIN_CPU_TIME) + 1));
+		else sleepytime = MIN_IO_TIME + (int)(Math.random() * ((MAX_IO_TIME - MIN_IO_TIME) + 1));
 		trackNumber = (int)(Math.random() * ((DiskDrive.DISK_SIZE) + 1));
+		System.out.println("trackNumber : " + trackNumber);
 	}
 
 	public void run() {
@@ -19,16 +27,11 @@ public class UserJob extends Thread {
 	}
 
 	public void work() {
-		for (int i = 0; i < 3; i++) {
-			
-			if (myJob == Job.CPU_BOUND) {
-				System.out.println("UserJob " + myName + " starting CPU burst of length " + sleepytime + ".");
-				try {sleep(sleepytime);} catch(Exception e) {};
-			} else {
-				DD.useTheDisk(trackNumber);
-				System.out.println("UserJob " + myName + " starting IO burst of length CPUtime " + sleepytime + ".");
-			}
-			
+		for (int i = 0; i < 3; i++) {			
+			if (myJob == Job.CPU_BOUND) System.out.println("UserJob " + myName + " starting CPU burst of length " + sleepytime + ".");
+			else System.out.println("UserJob " + myName + " starting IO burst of length CPUtime " + sleepytime + ".");
+			try {sleep(sleepytime);} catch(Exception e) {};
+			DD.useTheDisk(trackNumber);			
 		}
 	}
 }
