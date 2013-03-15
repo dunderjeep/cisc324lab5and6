23b40879e577707f2d3d4557c82c6bb35684060d
dunderjeep <dunderjeep@gmail.com>
2013-03-15 18:54:10 -0400

    updated useTheDisk with wait and notify

--- DiskDrive.java
+++ DiskDrive.java
@@ -1,16 +1,24 @@
 public class DiskDrive {
-	private int track = 0;
+	private int track;
 	private int moveTime;
 	private static final int READ_TIME = 1;
 	public static final int DISK_SIZE = 1024;
+	
+	private int userCount;
 		
 	public DiskDrive() {
-
+		userCount = 0;
 	}
 
 	public synchronized void useTheDisk(int t) {
+		while (userCount > 0) try {  wait(); } catch (Exception e) {};  // wait for "notify()"
+		userCount++; 	
+		System.out.println("userCount: " +userCount);	
 		moveTime = Math.abs(t - track);	
-		System.out.println("Disk is at track number " + track + ", and will move " + " tracks taking " + t + 1 + "  milliseconds.");			
+		System.out.println("Disk is at track number " + track + ", and will move " + " tracks taking " + (t + 1) + "  milliseconds.");			
 		try {Thread.sleep(moveTime + READ_TIME);} catch(Exception e) {};
+		userCount--;
+		System.out.println("userCount: " +userCount);
+		notify();
 	}
 }
--- UserJob.java
+++ UserJob.java
@@ -5,9 +5,9 @@ public class UserJob extends Thread {
 	public static final int MIN_IO_TIME = 1;
 	public static final int MAX_IO_TIME = 200;
 
-	int myName;
-	int sleepytime;
-	int trackNumber;
+	private int myName;
+	private int sleepytime;
+	public int trackNumber;
 	public enum Job { CPU_BOUND, IO_BOUND};
 	Job myJob;
 	DiskDrive DD;
@@ -16,9 +16,9 @@ public class UserJob extends Thread {
 		myName = name;	
 		myJob = job;
 		if (job == Job.CPU_BOUND) sleepytime = MIN_CPU_TIME + (int)(Math.random() * ((MAX_CPU_TIME - MIN_CPU_TIME) + 1));
-		else sleepytime = MIN_IO_TIME + (int)(Math.random() * ((MAX_IO_TIME - MIN_IO_TIME) + 1));
+		else sleepytime = MIN_IO_TIME + (int)(Math.random() * ((MAX_IO_TIME - MIN_IO_TIME) + 1));						
 		trackNumber = (int)(Math.random() * ((DiskDrive.DISK_SIZE) + 1));
-		System.out.println("trackNumber : " + trackNumber);
+		DD = new DiskDrive();
 	}
 
 	public void run() {
@@ -27,11 +27,13 @@ public class UserJob extends Thread {
 	}
 
 	public void work() {
-		for (int i = 0; i < 3; i++) {			
+		for (int i = 0; i < 3; i++) {	
 			if (myJob == Job.CPU_BOUND) System.out.println("UserJob " + myName + " starting CPU burst of length " + sleepytime + ".");
 			else System.out.println("UserJob " + myName + " starting IO burst of length CPUtime " + sleepytime + ".");
 			try {sleep(sleepytime);} catch(Exception e) {};
-			DD.useTheDisk(trackNumber);			
+			System.out.println("User " + myName + " requesting to access disk track " + trackNumber);
+			DD.useTheDisk(trackNumber);
+			System.out.println("User " + myName + " finished reading disk track " + trackNumber);			
 		}
 	}
 }
