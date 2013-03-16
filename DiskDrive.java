a8e5f866f15d5f482456b3cc6d9ab5b46c2e4a80
dunderjeep <dunderjeep@gmail.com>
2013-03-16 12:02:12 -0400

    put oldTrack number static/global fixing track# problem

--- DiskDrive.java
+++ DiskDrive.java
@@ -1,5 +1,5 @@
 public class DiskDrive {
-	private int track;
+	public static int oldTrack;
 	private int moveTime;
 	private static final int READ_TIME = 1;
 	public static final int DISK_SIZE = 1024;
@@ -10,12 +10,13 @@ public class DiskDrive {
 		userCount = 0;
 	}
 
-	public synchronized void useTheDisk(int t) {
-		while (userCount > 0) try {  wait(); } catch (Exception e) {};  // wait for "notify()"
-		userCount++; 	
-		System.out.println("userCount: " +userCount);	
-		moveTime = Math.abs(t - track);	
-		System.out.println("Disk is at track number " + track + ", and will move " + " tracks taking " + (t + 1) + "  milliseconds.");			
+	public synchronized void useTheDisk(int newTrack) {
+		System.out.println("newTrack: "+newTrack+" oldTrack: "+oldTrack);
+		while (userCount > 0) try { wait(); } catch (Exception e) {};  // wait for "notify()"
+		userCount++; 		
+		moveTime = Math.abs(newTrack - oldTrack);			
+		System.out.println("Disk is at track number " + oldTrack + ", and will move " + moveTime + " tracks taking " + (moveTime + 1) + "  milliseconds.");			
+		oldTrack = newTrack;
 		try {Thread.sleep(moveTime + READ_TIME);} catch(Exception e) {};
 		userCount--;
 		System.out.println("userCount: " +userCount);
