a85f6d25720573956bab323ff49c0fc8aa5806f2
dunderjeep <dunderjeep@gmail.com>
2013-03-18 20:55:40 -0400

    used ArrayList in CPUmonitor getting IndexOutofBoundsException

--- MainMethod.java
+++ MainMethod.java
@@ -1,10 +1,12 @@
 public class MainMethod {
+	
+	public CPUmonitor cpuMonitor;
 
   	public static void main (String argv[]) {
-	int numberOfProcesses = 6;	
+	int numberOfProcesses = 6;
+	int numberOfCPUs = 3;
 	UserJob[] processes = new UserJob[numberOfProcesses]; 
-	UserJob.Job job;	 	
-	
+	UserJob.Job job;
 	for (int i=0; i<6; i++) {
 		if (i % 2 == 0) processes[i] = new UserJob(i, UserJob.Job.CPU_BOUND);
 		else processes[i]= new UserJob(i, UserJob.Job.IO_BOUND);
--- UserJob.java
+++ UserJob.java
@@ -19,7 +19,7 @@ public class UserJob extends Thread {
 		if (job == Job.CPU_BOUND) sleepytime = MIN_CPU_TIME + (int)(Math.random() * ((MAX_CPU_TIME - MIN_CPU_TIME) + 1));
 		else sleepytime = MIN_IO_TIME + (int)(Math.random() * ((MAX_IO_TIME - MIN_IO_TIME) + 1));						
 		trackNumber = (int)(Math.random() * ((DiskDrive.DISK_SIZE) + 1));
-		CPU = new CPUmonitor(1,1);
+		CPU = new CPUmonitor(3);
 		DD = new DiskDrive();
 	}
 
@@ -32,9 +32,9 @@ public class UserJob extends Thread {
 		for (int i = 0; i < 3; i++) {	
 			if (myJob == Job.CPU_BOUND) System.out.println("UserJob " + myName + " starting CPU burst of length " + sleepytime + ".");
 			else System.out.println("UserJob " + myName + " starting IO burst of length CPUtime " + sleepytime + ".");
-//			CPU.startCPUuse(i);
+			CPU.startCPUuse(myName);
 			try {sleep(sleepytime);} catch(Exception e) {};
-//			CPU.endCPUuse(i);
+			CPU.endCPUuse(myName);
 			System.out.println("User " + myName + " requesting to access disk track " + trackNumber);
 			DD.useTheDisk(trackNumber);
 			System.out.println("User " + myName + " finished reading disk track " + trackNumber);			
